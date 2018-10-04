1a064d100982ed3c8f54a198ac962724e9245b7f
Nate <nathanestes@gmail.com>
2018-10-03 23:15:26 -0600

    added resource in contactform.js

--- Scripts/contactform.js
+++ Scripts/contactform.js
@@ -2,3 +2,5 @@ document.getElementById('textbox_id').value
 
 
 https://www.formget.com/javascript-contact-form/
+
+https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Sending_forms_through_JavaScript
